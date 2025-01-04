5789cb09bcd4ecaba5447c781c416f5ef18d6341
Anna Pollard <annapollard@MacBook-Pro.lan>
2025-01-04 11:17:34 -0500

    HeadingBox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
 <script>
+    import HeadingBox from '../lib/HeadingBox.svelte';
     import Hero from '../lib/Hero.svelte';
 </script>
 
 <Hero />
+
+<section>
+    <div class="outer bg-gradient-gray">
+        <div class="inner">
+            <HeadingBox heading="Check out our most popular categories!" />
+        </div>
+    </div>
+</section>
+
+<style>
+ .inner {
+    padding-block: 8rem;
+ }
+</style>
