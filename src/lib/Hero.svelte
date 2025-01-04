e47e936705656f1aa29a77b2e44fb7ae8d36d67a
Anna Pollard <annapollard@MacBook-Pro.lan>
2025-01-04 08:43:01 -0500

    Hero

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,30 @@
+<section>
+    <div class="outer">
+        <div class="inner">
+            <div class="hero-content bg-gradient-blue white flow">
+            <h1>Lorem, ipsum dolor</h1>
+            <p class="white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Corrupti quisquam culpa assumenda, expedita labore illum sit!</p>
+                <a href="/blog" class="button bg-gradient-pink white">Get Started</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style>
+.outer {
+    background-image: url(https://picsum.photos/1600/600);
+    background-size: cover;
+    background-position: center;
+}
+
+.inner {
+    padding-block: 8rem; 
+}
+
+.hero-content {
+    max-width: 32rem;
+    padding: 3rem;
+    border-radius: 1rem;  
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,5 @@
-<section>
-    <div class="outer">
-        <div class="inner">
-            <div class="hero-content bg-gradient-blue white flow">
-            <h1>Lorem, ipsum dolor</h1>
-            <p class="white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Corrupti quisquam culpa assumenda, expedita labore illum sit!</p>
-                <a href="/blog" class="button bg-gradient-pink white">Get Started</a>
-            </div>
-        </div>
-    </div>
-</section>
+<script>
+    import Hero from '../lib/Hero.svelte';
+</script>
 
-<style>
-.outer {
-    background-image: url(https://picsum.photos/1600/600);
-    background-repeat: cover;
-    background-position: center;
-}
-
-.inner {
-    padding-block: 8rem; 
-}
-
-.hero-content {
-    max-width: 32rem;
-    padding: 3rem;
-    border-radius: 1rem;  
-}
-</style>
+<Hero />
