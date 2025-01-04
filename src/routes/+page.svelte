d3a386d3c189de0846e3e324a9a2e690adef9904
Anna Pollard <annapollard@MacBook-Pro.lan>
2025-01-04 15:14:31 -0500

    version 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Box from '../lib/Box.svelte';
     import HeadingBox from '../lib/HeadingBox.svelte';
     import Hero from '../lib/Hero.svelte';
 </script>
@@ -7,8 +8,13 @@
 
 <section>
     <div class="outer bg-gradient-gray">
-        <div class="inner">
+        <div class="inner auto-fit">
             <HeadingBox heading="Check out our most popular categories!" />
+            <Box heading="Animation" src="icons/icon-animation.svg" />
+            <Box heading="Business" src="icons/icon-business.svg" />
+            <Box heading="Design" src="icons/icon-design.svg" />
+            <Box heading="Photography" src="icons/icon-photography.svg" />
+            <Box heading="Programming" src="icons/icon-programming.svg" />
         </div>
     </div>
 </section>
