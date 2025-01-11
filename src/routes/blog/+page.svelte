3221110d5a3b308dc66195260aed7b0be0e7f3f5
Anna Pollard <annapollard@MacBook-Pro.lan>
2025-01-11 09:37:46 -0500

    displayed bolg posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,3 +6,15 @@
 
 <Title name="Blog" />
 
+<section>
+    <div class="outer">
+        <div class="inner auto-fit">
+            {#each data.posts as post}
+                <div class="flow">
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            {/each}
+        </div>
+    </div>
+</section>
